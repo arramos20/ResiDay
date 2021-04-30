c1382e4b07f3ed5ef4f7534c5c9b45d90de40800
Alejandro Ramirez <ramirez.raale20@triana.salesianos.edu>
2021-04-30 08:19:20 +0200

    Subido proyecto generado

---
+++ ResiDay/src/main/java/com/arramos/ResiDay/ResiDayApplication.java
@@ -0,0 +1,13 @@
+package com.arramos.ResiDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResiDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResiDayApplication.class, args);
+	}
+
+}
---
+++ ResiDay/src/test/java/com/arramos/ResiDay/ResiDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arramos.ResiDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResiDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
