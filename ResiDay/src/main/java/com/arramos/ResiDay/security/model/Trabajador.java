2edfdefc024eece0cc13b894ce4d6d5899818298
Alejandro Ramirez <ramirez.raale20@triana.salesianos.edu>
2021-04-30 08:54:24 +0200

    modelos Usuario y modelo Trabajador en seguridad implementadas

--- ResiDay/src/main/java/com/arramos/ResiDay/security/model/Trabajador.java
+++ ResiDay/src/main/java/com/arramos/ResiDay/security/model/Trabajador.java
@@ -1,5 +1,35 @@
 package com.arramos.ResiDay.security.model;
 
-public class Trabajador {
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import javax.persistence.Entity;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collection;
+
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@NoArgsConstructor
+@Entity
+public class Trabajador extends Usuario{
+
+    private static final long serialVersionUID = 1L;
+
+    private LocalDate fechaAlta;
+    private String nombrePuesto;
+
+    public Trabajador(String nombre, String apellidos, String email, String password, LocalDate fechaAlta, String nombrePuesto) {
+        super(nombre, apellidos, email, password);
+        this.fechaAlta = fechaAlta;
+        this.nombrePuesto = nombrePuesto;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
 }
